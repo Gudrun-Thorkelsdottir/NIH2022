9f5e5dc714308139cd07256db2518d71732bb366
Gudrun-Thorkelsdottir <gudrun2001@gmail.com>
2022-07-21 14:41:21 -0400

    fixed

--- reorder.py
+++ reorder.py
@@ -4,6 +4,6 @@ import pandas as pd
 target = pd.read_csv(sys.argv[1])
 reorder = pd.read_csv(sys.argv[2])
 
-reorder.columns = target.keys()
+reorder = reorder[target.keys()]
 reorder = reorder.rename({"Unnamed: 0": ""}, axis='columns')
 reorder.to_csv(sys.argv[2], index=False)
